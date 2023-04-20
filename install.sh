c8c28f851f4f30fffa6e98696fd779f02fd27211
Son Yen Nguyen <nosyn@Sons-MBP.attlocal.net>
2023-04-19 23:05:01 -0500

    Update dotfiles

--- install.sh
+++ install.sh
@@ -16,10 +16,6 @@ chsh -s $(which zsh)
 # Installing zap - Plugins manager for ZSH
 sh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.sh)
 
-# Installing AstroNvim
-echo "Installing astronvim"
-git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-
 # Installing fzf
 echo "Installing fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -28,13 +24,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # Installing thefuck
 apt install python3-dev python3-pip python3-setuptools
 pip3 install thefuck --user
-
-# Installing tmux
-apt install tmux
-tmux source ~/.tmux.conf`
-
-# Installing i3
-apt install i3
 
 # Installing neofetch
 apt install neofetch
